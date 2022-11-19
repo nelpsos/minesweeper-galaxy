6ceaa4ee95b3170659aba8a531be2373297a84f4
LeeJangPyo <65614770+LeeJangPyo@users.noreply.github.com>
2022-11-20 08:32:00 +0900

    2022-11-20

--- Assets/Scripts/Controllers/CellController.cs
+++ Assets/Scripts/Controllers/CellController.cs
@@ -4,28 +4,46 @@ using UnityEngine;
 
 public class CellController : MonoBehaviour
 {
-
-    public Define.CellState CellState
-    {
-        get
-        {
-            return CellState;
-        }
-        set
-        {
-            CellState = value;
-        }
-    }
+    public Define.CellState CellState { get; set; }
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        Managers.Input.MouseAction -= OnMouseClicked;
+        Managers.Input.MouseAction += OnMouseClicked;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SetCellState(int mineCount)
+    {
+        CellState = (Define.CellState)mineCount;
+
+        SetColor(CellState);    
+    }
+
+    public void SetCellState(Define.CellState cellState)
+    {
+        CellState = cellState;
+
+        SetColor(CellState);
+    }
+
+    public void SetColor(Define.CellState cellState)
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null)
+            Debug.Assert(true, "Set Color Function - GetCompoent Fail");
+
+        spriteRenderer.color = Util.GetCellStateColor(cellState);
+    }
+
+    void OnMouseClicked(Define.MouseEvent evt)
+    {
+
     }
 }
--- Assets/Scripts/Managers/Core/InputManager.cs
+++ Assets/Scripts/Managers/Core/InputManager.cs
@@ -9,6 +9,7 @@ public class InputManager
     public Action KeyAction = null;
     public Action<Define.MouseEvent> MouseAction = null;
     public bool _pressed = false;
+    public bool _RPress = false;
 
     public void OnUpdate()
     {
@@ -20,16 +21,28 @@ public class InputManager
 
         if (MouseAction != null)
         {
+            
             if (Input.GetMouseButton(0))
             {
+                //좌 버튼 다운
                 MouseAction.Invoke(Define.MouseEvent.Press);
                 _pressed = true;
+            }
+            else if(Input.GetMouseButton(1))
+            {
+                //우버튼 클릭
+                MouseAction.Invoke(Define.MouseEvent.RPress);
+                _RPress = true;
             }
             else
             {
                 if (_pressed)
                     MouseAction.Invoke(Define.MouseEvent.Click);
                 _pressed = false;
+
+                if(_RPress)
+                    MouseAction.Invoke(Define.MouseEvent.RClick);
+                _RPress = false;
             }
         }
     }
